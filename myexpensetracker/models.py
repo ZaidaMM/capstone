17116fb8c3ce8aea304d39a9992103fe041e8a1f
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2024-02-16 23:31:29 +0000

    First commit

---
+++ myexpensetracker/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Expense(models.Model):
+  item = models.CharField(max_length=200)
+  amount = models.DecimalField(max_digits=10, decimal_places=2)
+  type = models.CharField(max_length=200)
+  timestamp = models.DateField(auto_now=True)
+
+  def __str__(self):
+    return f"{self.item},  £{self.amount}, {self.type}, {self.timestamp}"
