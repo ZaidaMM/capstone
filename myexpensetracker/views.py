81b7cbe553894da29e89e85323a24df9622696d2
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2024-02-17 03:25:04 +0000

    Expenses table setup

--- myexpensetracker/views.py
+++ myexpensetracker/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from .models import Expense
 from .forms import ExpenseForm
 
 # Create your views here.
 def index(request):
+  if request.method == "POST":
+    item = ExpenseForm(request.POST)
+    if item.is_valid():
+      item.save()
+
+
+  items = Expense.objects.all()
   expense_form = ExpenseForm()
-  return render(request, 'myexpensetracker/index.html', {'expense_form':expense_form})
+  return render(request, 'myexpensetracker/index.html', {'expense_form':expense_form, 'items':items})
