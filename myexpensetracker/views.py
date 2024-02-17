2a0e7a95712a612cfd9dcbba9695dcf0edfea3bc
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2024-02-17 02:19:31 +0000

    Expense form setup

--- myexpensetracker/views.py
+++ myexpensetracker/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .forms import ExpenseForm
 
 # Create your views here.
 def index(request):
-  return render(request, 'myexpensetracker/index.html')
+  expense_form = ExpenseForm()
+  return render(request, 'myexpensetracker/index.html', {'expense_form':expense_form})
