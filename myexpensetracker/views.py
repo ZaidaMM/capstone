770851c09600ee22df9a3b7e6b50cc65f0c80712
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2024-02-26 15:17:32 +0000

    Recent expenses and expenses by type setup

--- myexpensetracker/views.py
+++ myexpensetracker/views.py
@@ -1,9 +1,15 @@
+import datetime
+from datetime import timedelta, date
 from django.shortcuts import redirect, render
 from django.db.models import Sum
 from .models import Expense
 from .forms import ExpenseForm
 
 # Create your views here.
+
+def days_ago(n):
+  return date.today() - timedelta(n)
+
 def index(request):
   if request.method == "POST":
     item = ExpenseForm(request.POST)
@@ -13,8 +19,26 @@ def index(request):
   items = Expense.objects.all()
   expense_form = ExpenseForm()
   amount_total = items.aggregate(Sum('amount'))
+
+  today_figure = days_ago(1)
+  data = Expense.objects.filter(timestamp__gt=today_figure)
+  today_total = data.aggregate(Sum('amount'))
+
+  last_seven_days = days_ago(7)
+  data = Expense.objects.filter(timestamp__gt=last_seven_days)
+  seven_day_total = data.aggregate(Sum('amount'))
+
+  last_three_months = days_ago(90)
+  data = Expense.objects.filter(timestamp__gt=last_three_months)
+  three_months_total = data.aggregate(Sum('amount'))
+
+  last_year = days_ago(365)
+  data = Expense.objects.filter(timestamp__gt=last_year)
+  last_year_total = data.aggregate(Sum('amount'))
   
-  return render(request, 'myexpensetracker/index.html', {'expense_form':expense_form, 'items':items, 'amount_total':amount_total})
+  expenses_by_type = Expense.objects.filter().values('type').order_by('type').annotate(Sum('amount'))
+  
+  return render(request, 'myexpensetracker/index.html', {'expense_form':expense_form, 'items':items, 'amount_total':amount_total, 'seven_day_total':seven_day_total, 'today_total':today_total, 'three_months_total':three_months_total, 'last_year_total':last_year_total, 'expenses_by_type':expenses_by_type})
 
 def edit(request,id):
   item = Expense.objects.get(id=id)
